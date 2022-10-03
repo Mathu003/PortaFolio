76d0102b2f8b18786558d594fe783b47c3916166
Mathu003 <Mathias_eze1@outlook.com>
2022-10-03 20:49:27 -0300

    comenzando main

---
+++ java.js
@@ -0,0 +1,26 @@
+/*function demoDisplay() {
+    document.getElementById("myDIV").style.display = "block";
+  }
+  setTimeout(demoDisplay,2000)
+
+  new Vue({
+    el: '#demo',
+    data: {
+      show: true
+    }
+  }) */
+
+  // Can also be included with a regular script tag
+
+  var typed6 = new Typed('.type', {
+    strings: ['Soy Desarrollador Web Full stack'],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: false
+  });
+  var typed7 = new Typed('.type2', {
+    strings: ['BIENVENIDOS'],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: false
+  });
